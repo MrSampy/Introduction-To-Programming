186092ec7fea4c802c6022c53750889f1b9ddec0
MrSampy <mynameisserzheo@gmail.com>
2022-04-02 23:52:59 +0300

    Add logic for Reading File
    -Creating a project Var 1 Lab 2
    -Reading file to array of chars

---
+++ labs_spring_2021/examples_3/Var 1/Var 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+char[,] matrix;
+var path="/home/serhiy/Документы/GitHub/Introduction-To-Programming/labs_spring_2021/examples_3/Labirint.csv";
+int rows= File.ReadAllLines(path).Length, columns=0;
+using (StreamReader reader = new StreamReader(path))
+{
+   string a = reader.ReadLine();
+   for (int i = 0; i < a.Length; i++)
+   { 
+      if (a[i] == ' ' && a[i + 1] == ' ' && a[i-1]=='X')
+         break;
+      ++columns;
+   }
+   matrix = new char [rows, columns];
+   reader.BaseStream.Position = 0;
+   reader.ReadToEnd();
+   reader.BaseStream.Position = 0;
+   for (int i = 0; i < rows; ++i)
+   {
+      for (int j = 0; j < columns; ++j)
+      {
+         matrix[i, j] = Convert.ToChar(reader.Read());
+      }
+      reader.ReadLine();
+   }
+}
+
+for (int i = 0; i < rows; ++i)
+{
+   for (int j = 0; j < columns; j++)
+   {
+      Console.Write($"{matrix[i,j]}");
+   }
+   Console.WriteLine();
+}
